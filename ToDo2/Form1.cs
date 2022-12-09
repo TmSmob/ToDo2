7776f2fd9ffa46dc3aecab2808edc6f927f3a493
ahmetrf <arifuu.rf@gmail.com>
2022-12-09 17:06:35 +0300

    rgb

--- ToDo2/Form1.cs
+++ ToDo2/Form1.cs
@@ -12,6 +12,10 @@ namespace ToDo2
 {
     public partial class Form1 : Form
     {
+        private Timer timer1;
+        private Label label1;
+        private IContainer components;
+
         public Form1()
         {
             InitializeComponent();
@@ -30,6 +34,51 @@ namespace ToDo2
         private void button3_Click(object sender, EventArgs e)
         {
             label1.Text = "Gule Gule Emirhan";
+        }
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(73, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        int a = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(a == 0)
+            this.BackColor = Color.Red;
+            else if(a == 1)
+            this.BackColor = Color.Blue;
+            else
+            {
+            this.BackColor = Color.Green;
+                a = 0;
+            }
+            a++;
         }
     }
 }
