ee8e4ec077a5826a99d5846ffd9236779f7b2df8
TmSmob <emir26han62@gmail.com>
2022-12-09 16:29:50 +0300

    df

--- ToDo2/Form1.Designer.cs
+++ ToDo2/Form1.Designer.cs
@@ -54,7 +54,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(93, 12);
+            this.button2.Location = new System.Drawing.Point(214, 12);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 2;
