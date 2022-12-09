2637e5d6f90c0e5fe4959fe7b1dac077ed796ca1
ahmetrf <arifuu.rf@gmail.com>
2022-12-09 16:53:54 +0300

    asd

--- ToDo2/Form1.Designer.cs
+++ ToDo2/Form1.Designer.cs
@@ -96,7 +96,7 @@
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(376, 100);
+            this.button6.Location = new System.Drawing.Point(401, 99);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(75, 23);
             this.button6.TabIndex = 6;
